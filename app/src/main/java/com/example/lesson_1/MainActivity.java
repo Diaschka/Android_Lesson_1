0a89d6184cb3e59ec1b87559a7f41b1f0b449edc
Diaschka <diana.snizinova@gmail.com>
2021-09-12 13:56:12 +0300

    Task 1.1

--- app/src/main/java/com/example/lesson_1/MainActivity.java
+++ app/src/main/java/com/example/lesson_1/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.lesson_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView usersName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //usersName.findViewById(R.id.name_edit_text);
+
     }
+
+
 }
